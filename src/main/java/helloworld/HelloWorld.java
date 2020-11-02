7997a95821d6943e77a224d4ea7e5d5b1df12aed
Artur Galimov <satorica@mail.ru>
2020-11-02 13:33:11 +0300

    Initial commit.

---
+++ src/main/java/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+public class HelloWorld{
+  public static void main (String[] args) {
+  JOptionPane.showMessageDialog (null, "Всем большой привет!");
+  }
+}
+
+//public class HelloWorld {
+//    public static void main (String[] args) {
+//    System.out.println("Всем большой привет!");
+//    }
+//}
